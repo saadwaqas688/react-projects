0c9d307eeb7fe47e595432d25e8f6b563be247af
saadwaqas688 <86394839+saadwaqas688@users.noreply.github.com>
2023-06-21 18:55:20 +0500

    Update Tour.js

--- 02-tours/final/src/Tour.js
+++ 02-tours/final/src/Tour.js
@@ -1,27 +1,21 @@
-import React, { useState } from 'react';
 
-const Tour = ({ id, image, info, name, price, removeTour }) => {
-  const [readMore, setReadMore] = useState(false);
+
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({ tours, removeTour }) => {
   return (
-    <article className="single-tour">
-      <img src={image} alt={name} />
-      <footer>
-        <div className="tour-info">
-          <h4>{name}</h4>
-          <h4 className="tour-price">${price}</h4>
-        </div>
-        <p>
-          {readMore ? info : `${info.substring(0, 200)}...`}
-          <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? 'show less' : '  read more'}
-          </button>
-        </p>
-        <button className="delete-btn" onClick={() => removeTour(id)}>
-          not interested
-        </button>
-      </footer>
-    </article>
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
   );
 };
 
-export default Tour;
+export default Tours;
